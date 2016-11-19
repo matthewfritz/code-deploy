3358579d5fae4a2224c63a62ec8ae8b09d81ef71
Matthew Fritz <matthew.fritz@csun.edu>
2016-11-19 13:30:32 -0800

    Made some progress in implementing the Strategy design pattern for deployments

---
+++ modules/Deployments/Strategies/DeploymentStrategyGitHub.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Deployments\Strategies;
+
+use Deployments\Strategies\Interfaces\DeploymentStrategyInterface;
+use Deployments\Models\DeploymentConfiguration;
+
+class DeploymentStrategyGitHub implements DeploymentStrategyInterface
+{
+	public function deploy(DeploymentConfiguration $config);
+}
