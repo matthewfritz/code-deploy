81c7ce802ba1bc4eacc766902e20a6747171c649
Matthew Fritz <matthew.fritz@csun.edu>
2016-11-19 13:31:46 -0800

    It would help if I could actually associate the correct namespaces with classes

--- modules/Deployments/Interfaces/DeploymentStrategyInterface.php
+++ modules/Deployments/Interfaces/DeploymentStrategyInterface.php
@@ -2,6 +2,8 @@
 
 namespace Deployment\Strategies;
 
+use Deployments\Models\DeploymentConfiguration;
+
 interface DeploymentStrategyInterface
 {
 	public function deploy(DeploymentConfiguration $config);
